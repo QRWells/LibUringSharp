d11d4fc86bdf0899f37d29620d0a16feffbe125c
Qirui Wang <qirui.wang@moegi.waseda.jp>
2023-01-24 04:15:29 +0900

    build: add empty source generator for prepares

---
+++ src/LibUringSharp.SourceGeneration/PrepareGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace LibUringSharp.SourceGeneration;
+
+// [Generator]
+public class PrepareGenerator : ISourceGenerator
+{
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Execute(GeneratorExecutionContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
