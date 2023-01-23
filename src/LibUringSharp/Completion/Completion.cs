c1444a2c400e79996d4a17b2dbdde4bd106a139c
Qirui Wang <qirui.wang@moegi.waseda.jp>
2023-01-24 04:03:06 +0900

    feat: add abstract for sqe and cqe

---
+++ src/LibUringSharp/Completion/Completion.cs
@@ -0,0 +1,17 @@
+﻿using Linux;
+
+namespace LibUringSharp.Completion;
+
+public readonly struct Completion
+{
+    public readonly int Result;
+    public readonly ulong UserData;
+    public readonly uint Flags;
+
+    public Completion(int res, ulong userData, uint flags)
+    {
+        Result = res;
+        UserData = userData;
+        Flags = flags;
+    }
+}
---
+++ src/LibUringSharp/Submission/Submission.cs
@@ -0,0 +1,26 @@
+﻿using LibUringSharp.Enums;
+using static Linux.LibC;
+
+namespace LibUringSharp.Submission;
+
+public readonly unsafe struct Submission
+{
+    private readonly io_uring_sqe* _sqe;
+
+    internal Submission(io_uring_sqe* sqe)
+    {
+        _sqe = sqe;
+    }
+
+    public void PrepareNop(ulong userData = 0, SqeOption options = SqeOption.None, ushort personality = 0)
+    {
+        unchecked
+        {
+            _sqe->opcode = (byte)IoUringOp.Nop;
+            _sqe->flags = (byte)options;
+            _sqe->fd = -1;
+            _sqe->user_data = userData;
+            _sqe->personality = personality;
+        }
+    }
+}
